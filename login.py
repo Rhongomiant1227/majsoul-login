abd570435422890a4bf8a314f5ee1e9dad42c3c2
Rhongomiant1227 <yuudachichanrobin@gmail.com>
2024-11-09 13:58:19 +0800

    Update login.py
    [email1, password1, email2, password2]

--- login.py
+++ login.py
@@ -5,24 +5,26 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
-
-acccounts = int(len(sys.argv[1:])/2)
+# 计算账号数量，每两个参数为一组账号信息
+acccounts = int(len(sys.argv[1:]) / 2)
 print(f'Config {acccounts} accounts')
+
 for i in range(acccounts):
-    email = sys.argv[1+i]
-    passwd = sys.argv[1+i+acccounts]
+    # 根据新的顺序，提取邮箱和密码
+    email = sys.argv[1 + 2 * i]
+    passwd = sys.argv[2 + 2 * i]
     print('----------------------------')
 
-    #1.open browser
+    # 1.打开浏览器
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     driver.set_window_size(1000, 720)
     driver.get("https://game.maj-soul.net/1/")
-    print(f'Account {i+1} loading game...')
+    print(f'Account {i + 1} loading game...')
     sleep(20)
 
-    #2.input email
+    # 2.输入邮箱
     screen = driver.find_element(By.ID, 'layaCanvas')
     ActionChains(driver)\
         .move_to_element_with_offset(screen, 250, -100)\
@@ -31,7 +33,7 @@ for i in range(acccounts):
     driver.find_element(By.NAME, 'input').send_keys(email)
     print('Input email successfully')
 
-    #3.input password
+    # 3.输入密码
     ActionChains(driver)\
         .move_to_element_with_offset(screen, 250, -50)\
         .click()\
@@ -39,12 +41,12 @@ for i in range(acccounts):
     driver.find_element(By.NAME, 'input_password').send_keys(passwd)
     print('Input password successfully')
 
-    #4.login
+    # 4.登录
     ActionChains(driver)\
         .move_to_element_with_offset(screen, 250, 50)\
         .click()\
         .perform()
     print('Entering game...')
-    sleep(20) #loading...
+    sleep(20)  # 加载中...
     print('Login success')
     driver.quit()
